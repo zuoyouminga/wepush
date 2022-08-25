c79ee79eb7adabb8ad91069ac490b1cda8a0f9ba
wxdlicc <51895547+wxdlicc@users.noreply.github.com>
2022-08-25 23:26:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ data = {"weather": {"value": wea, "color": get_random_color()},
         "love_days": {"value": get_count(), "color": get_random_color()},
         "birthday_left": {"value": get_birthday(), "color": get_random_color()},
         "words": {"value": get_words(), "color": get_random_color()}}
-ptint(data)
+print(data)
 res = wm.send_template(user_id, template_id, data)
 print(res)
 res_my = wm.send_template(my_id, template_id, data)
