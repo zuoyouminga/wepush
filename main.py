d3b0cbe2bcadf910ae4bd2bafa7b0b7571dc56bf
zuoyouminga <111970524+zuoyouminga@users.noreply.github.com>
2022-09-14 15:02:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_weather():
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days + 1
+  return delta.days
 
 def get_birthday():
     month = int(birthday[:2])
@@ -39,7 +39,7 @@ def get_birthday():
     next = ZhDate(today.year, month, day).to_datetime()
     if next < datetime.now():
         next = next.replace(year=next.year + 1)
-    return (next - today).days + 1
+    return (next - today).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
